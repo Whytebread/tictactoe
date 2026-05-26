ab324e42981f04030fba46f7469fd44130ed6068
Whytebread <196115419+Whytebread@users.noreply.github.com>
2026-05-26 16:29:03 -0500

    created gameboard function with board as a blank array, made create player factory with name and symbol

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 // GAMEBOARD
-const gameBoard =
+const gameBoard = () => {
+   let board =
     [
         ['','',''],
         ['','',''],
         ['','','']
     ];
+},
 
 // PLAYERS
-
-
-
+function createPlayer(name, symbol) {
+    return {
+        name: name,
+        symbol: symbol,
+    }
+};
 
 // GAME LOGIC
